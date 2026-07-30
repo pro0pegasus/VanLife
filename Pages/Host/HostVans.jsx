dd4a02a1913816f087db5cac81d086d973918146
Yassine Borak <yassineborak@gmail.com>
2026-07-30 03:44:46 +0100

    added filter section

--- Pages/Host/HostVans.jsx
+++ Pages/Host/HostVans.jsx
@@ -16,7 +16,7 @@ export default function HostVans(){
     
     const vansElement = vans.map(van=>(
         <Link
-            to={`/host/vans/${van.id}`}
+            to={van.id}
             key={van.id}
             className="host-van-link-wrapper"
         >
